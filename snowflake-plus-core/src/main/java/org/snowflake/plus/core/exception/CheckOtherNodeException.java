f86c260a37342a734ce0fd438b006bb6038460b8
onevroad <269807929@qq.com>
2019-07-18 16:40:24 +0800

    init

--- snowflake-plus-core/src/main/java/org/snowflake/plus/core/exception/CheckOtherNodeException.java
+++
@@ -1,7 +0,0 @@
-package org.snowflake.plus.core.exception;
-
-public class CheckOtherNodeException extends RuntimeException {
-    public CheckOtherNodeException(String message) {
-        super(message);
-    }
-}
--- snowflake-plus-core/src/main/java/org/snowflake/plus/core/exception/ClockGoBackException.java
+++
@@ -1,7 +0,0 @@
-package org.snowflake.plus.core.exception;
-
-public class ClockGoBackException extends RuntimeException {
-    public ClockGoBackException(String message) {
-        super(message);
-    }
-}
